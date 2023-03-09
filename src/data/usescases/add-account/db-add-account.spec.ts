ff98c8f0241614969c8ae34694a9dd81f6eb188a
Ivan A. Ferreira Aguiar <ivnagst@gmail.com>
2023-03-08 23:33:17 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usescases/add-account/db-add-account.spec.ts
+++ src/data/usescases/add-account/db-add-account.spec.ts
@@ -5,17 +5,20 @@ interface SutTypes {
   sut: DbAddAccount;
   encrypterStub: Encrypter;
 }
-const makeSut = (): any => {
-  class EncrypterStub {
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
     async encrypt(value: string): Promise<string> {
       return new Promise((resolve) => resolve('hashed_password'));
     }
   }
-  const encrypterStub = new EncrypterStub();
+  return new EncrypterStub();
+};
+const makeSut = (): any => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
   return {
-    sut,
     encrypterStub,
+    sut,
   };
 };
 
